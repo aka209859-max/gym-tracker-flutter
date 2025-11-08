d2535bde0186f356adde00dddaa4267953ff6220
GYM MATCH AI Dev <ai-dev@gymmatch.jp>
2025-11-08 17:02:06 +0000

    Fix: kDebugModeのためにfoundation.dartをインポート

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
